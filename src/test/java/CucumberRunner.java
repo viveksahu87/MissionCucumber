ae56df24891f8045c783230f7d9d25cb2ad80730
viveksahu87 <82751754+viveksahu87@users.noreply.github.com>
2021-07-22 15:18:06 +0530

    9
    9

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -4,11 +4,12 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class) // this inform java to run the below class with cucumber framework
 //@Cucumber.Options(features="Features", format = {"pretty","html:reports/test-report"}) // this tells where the feature scripts /tc scripts are kept
-//@CucumberOptions(plugin = {"pretty", "html:target/report.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},features={"src/test/resources/Features/Login.feature"},monochrome = true) // this tells where the feature scripts /tc scripts are kept
+//@CucumberOptions(plugin = {"pretty", "html:target/report.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},features={"src/test/resources/Features/Login.featureonhold"},monochrome = true) // this tells where the feature scripts /tc scripts are kept
 @CucumberOptions(plugin = {"pretty",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
-        features={"src/test/resources/Features/Login.feature"},
-        tags= "@smoke",monochrome = true)
+        features={"src/test/resources/Features"},
+  //      tags= "@smoke",monochrome = true)
+        monochrome = true)
 public class CucumberRunner {
 
 
---
+++ src/test/java/carSearch.java
@@ -0,0 +1,63 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class carSearch {
+
+
+    @Given("I am on the home page of carsguide website -> http:\\/\\/www.carsguide.com.au")
+    public void i_am_on_the_home_page_of_carsguide_website_http_www_carsguide_com_au() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @When("I move to car for sale menu")
+    public void i_move_to_car_for_sale_menu() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I click on Search Cars")
+    public void i_click_on_search_cars() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I select make as {string}")
+    public void i_select_make_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I select model as {string}")
+    public void i_select_model_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I select location as {string}")
+    public void i_select_location_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I select price as {string}")
+    public void i_select_price_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I click on Find My Next Car Button")
+    public void i_click_on_find_my_next_car_button() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("I should see list of searched cars")
+    public void i_should_see_list_of_searched_cars() {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    @Then("the page title should be {string}")
+    public void the_page_title_should_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+       // throw new io.cucumber.java.PendingException();
+    }
+    
+    
+}
