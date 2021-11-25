106b016d7dac3e099a7f0b73b44f695d13e67030
viveksahu87 <82751754+viveksahu87@users.noreply.github.com>
2021-11-25 20:58:14 +0530

    66
    6

--- src/test/java/LoginFB.java
+++ src/test/java/LoginFB.java
@@ -3,24 +3,34 @@ import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class LoginFB {
 
-    public static WebDriver driver;
-    public static int i =0;
+    //public static WebDriver driver;
+    public static int i = 0;
+    public static RemoteWebDriver driver;
+
     @Before
-    public void setup(){
+    public void setup() throws MalformedURLException {
         //initializa driver
-    //    System.out.println("path: " + System.getProperty("user.dir") + "***"+System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
+        //    System.out.println("path: " + System.getProperty("user.dir") + "***"+System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
+
+        DesiredCapabilities dc = DesiredCapabilities.chrome();
+        URL url = new URL("http://localhost:4444/wd/hub");
+        driver = new RemoteWebDriver(url, dc);
+       /* System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
         driver =new ChromeDriver();
 
+*/
 
     }
+
     @Given("User is on facebook page")
     public void user_is_on_facebook_page() {
         // Write code here that turns the phrase above into concrete actions
@@ -28,33 +38,33 @@ public class LoginFB {
         driver.get("https://www.makemytrip.com/");
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-     //   throw new io.cucumber.java.PendingException();
+        //   throw new io.cucumber.java.PendingException();
     }
 
     @When("user enter {string}")
     public void user_enter(String string) {
         System.out.println("String u or p  used: " + string);
         // Write code here that turns the phrase above into concrete actions
-      //  throw new io.cucumber.java.PendingException();
+        //  throw new io.cucumber.java.PendingException();
     }
 
     @When("^user click on login button$")
     public void user_click_on_login_button() {
-     System.out.println("TRY TRY UNTIL YOU SUCCEED");
+        System.out.println("TRY TRY UNTIL YOU SUCCEED");
         // Write code here that turns the phrase above into concrete actions
-       // throw new io.cucumber.java.PendingException();
+        // throw new io.cucumber.java.PendingException();
     }
 
     @Then("validate successful login message")
     public void validate_successful_login_message() {
         // Write code here that turns the phrase above into concrete actions
-       // throw new io.cucumber.java.PendingException();
+        // throw new io.cucumber.java.PendingException();
     }
 
     @Then("user is able to see profile page")
     public void user_is_able_to_see_profile_page() {
         // Write code here that turns the phrase above into concrete actions
-     //   throw new io.cucumber.java.PendingException();
+        //   throw new io.cucumber.java.PendingException();
     }
 
    /* @When("user enter username and password")
@@ -65,15 +75,15 @@ public class LoginFB {
     @When("user enter {string} and {string}")
     public void user_enter_and(String string, String string2) {
         System.out.println("username used: " + string + "| Password used is : " + string2);
-        System.out.println("iteration : " + i++ );
+        System.out.println("iteration : " + i++);
 
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
-       // System.out.println("final value of iteration : " + i );
-        }
+        // System.out.println("final value of iteration : " + i );
+    }
 
 
 }
