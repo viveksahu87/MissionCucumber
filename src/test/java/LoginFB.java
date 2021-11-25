560024762e4e05b8b5cce85ec92a15a7e5cd2beb
viveksahu87 <82751754+viveksahu87@users.noreply.github.com>
2021-11-25 17:46:28 +0530

    5
    5

--- src/test/java/LoginFB.java
+++ src/test/java/LoginFB.java
@@ -16,7 +16,7 @@ public class LoginFB {
     public void setup(){
         //initializa driver
     //    System.out.println("path: " + System.getProperty("user.dir") + "***"+System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver");
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
         driver =new ChromeDriver();
 
 
